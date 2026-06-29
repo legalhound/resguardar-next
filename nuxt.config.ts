ad8b2ea4cea62a703457fbcccda38bd9fcd769f6
Silvia  Nestares <silvianestares@gmail.com>
2026-06-29 12:55:20 +0200

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
         },
+        { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.png' }
       ],
     },
   },
